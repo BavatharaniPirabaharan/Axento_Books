a69c2e91870bdb04143bc3c863e7823976cb99fb
BavatharaniPirabaharan <tharapiraba123@gmail.com>
2025-03-20 13:03:55 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+//Express
+const express = require('express');
+const app = express();
+
+
+
+//Create a new axento item
+
+
+
+//start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log("Server is listening to port "+port);
+})
